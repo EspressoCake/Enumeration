b1b0fb6ba51dab8a8779514ed375a9196d7ea6e5
root <lucas.justin.k@gmail.com>
2016-10-10 15:08:33 -0400

    Added Lambda Sorting

--- BannerGrab.py
+++ BannerGrab.py
@@ -5,14 +5,16 @@ import sys
 from tabulate import tabulate
 import ipaddress
 
+
 def list_creation():
     data = []
     with open(sys.argv[1], 'r') as file:
         for line in file.readlines():
             initial_data = (i for i in ipaddress.ip_network(u'{}'.format(line.strip())))
             data.extend(map(str, initial_data))
-        file.close()
-    return data
+            file.close()
+    return sorted(data, key=lambda x:tuple(map(int, x.split('.'))))
+
 
 def grab_banner(ip_address):
     try:
@@ -31,8 +33,11 @@ if __name__ == '__main__':
         print "Usage: python {} CIDR_HOST_LIST PORT".format(sys.argv[0])
         sys.exit()
     else:
+        ip_address = list_creation()
         pool = Pool(500)
         ip_address = list_creation()
         datapoint = pool.map(grab_banner, ip_address)
         print tabulate([x for x in datapoint if x is not None], headers=["IP_ADDRESS", "Banner"])
-        ip_address = list_creation()
+
+
+
