dcc9764098a5f887a14053e24f4523c9500c1fd0
root <lucas.justin.k@gmail.com>
2016-10-10 17:36:55 -0400

    Added Option Parsing

--- BannerGrab.py
+++ BannerGrab.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
 import socket
 from multiprocessing import Pool
+from optparse import OptionParser
 import sys
 from tabulate import tabulate
 import ipaddress
 
 
+parser = OptionParser()
+usage = "USAGE: %prog [options]"
+parser.add_option("-i", type="string", help="Required Input CIDR File", dest="infile")
+parser.add_option("-n", type="int", help="Required Port To Scan", dest="port")
+parser.add_option("-s", type="string", help="Required Banner String", dest="banner")
+parser.add_option("-f", type="string", help="Optional Output File", dest="outfile")
+
+(options, args) = parser.parse_args()
+
+
 def list_creation():
     data = []
-    with open(sys.argv[1], 'r') as file:
+    with open(options.infile, 'r') as file:
         for line in file.readlines():
             initial_data = (i for i in ipaddress.ip_network(u'{}'.format(line.strip())))
             data.extend(map(str, initial_data))
@@ -19,25 +30,30 @@ def list_creation():
 def grab_banner(ip_address):
     try:
         s = socket.socket()
-        s.settimeout(5)
-        s.connect((ip_address, int(sys.argv[2])))
+        s.settimeout(2)
+        s.connect((ip_address, options.port))
         banner = s.recv(1024)
         s.close()
-        if "cryptlib" in banner:
+        if options.banner in banner:
             return [ip_address, banner.strip()]
     except Exception:
         pass
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print "Usage: python {} CIDR_HOST_LIST PORT".format(sys.argv[0])
-        sys.exit()
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+    if not all((options.port, options.banner, options.infile)):
+        parser.error("Please Supply All Required Arguments!")
     else:
         ip_address = list_creation()
-        pool = Pool(500)
-        ip_address = list_creation()
+        pool = Pool(1500)
+        print "\n\nIPs In Queue: {}\n\n".format(len(ip_address))
         datapoint = pool.map(grab_banner, ip_address)
-        print tabulate([x for x in datapoint if x is not None], headers=["IP_ADDRESS", "Banner"])
-
-
+        tableData = tabulate([x for x in datapoint if x is not None], headers=["IP_ADDRESS", "Banner"], tablefmt="grid")
+        if options.outfile:
+            with open(options.outfile, 'w') as outfile:
+                outfile.write(tableData)
+                outfile.close()
+        else:
+            print tableData
 
